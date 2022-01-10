d8713cfcec1ed920ea1d44c20ccb6d657ddbc07b
LightSof <62741914+depresdkid@users.noreply.github.com>
2022-01-10 19:00:27 +0300

    Подрезал библиотеки

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import telebot
-import d2api
-from telebot import types
-import random
+from d2api import APIWrapper
+from random import randint
 import config as cf
 
-api = d2api.APIWrapper(cf.STEAM_TOKEN)
+api = APIWrapper(cf.STEAM_TOKEN)
+
 heroes = api.get_heroes(language='ru', itemizedonly=True).get('heroes')
-
-
 
 bot = telebot.TeleBot(cf.TELEGRAM_TOKEN)
 
-markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-rand_bun = types.KeyboardButton('Рандом')
+markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+rand_bun = telebot.types.KeyboardButton('Рандом')
 
 markup.add(rand_bun)
-
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -23,9 +20,9 @@ def send_welcome(message):
 
 @bot.message_handler(content_types=['text'])
 def send_comand(message):
-
     if(message.text == 'Рандом'):
-        count_hero = random.randint(0, len(heroes))
+        count_hero = randint(0, len(heroes))
         bot.send_message(message.chat.id, heroes[count_hero].get('localized_name'))
 
 bot.polling(non_stop=True)
+
