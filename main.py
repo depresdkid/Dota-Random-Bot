d951a1bece2c3701b8511ee2ef015d2128e2b514
Teilat <kot.sergej2012@yandex.ru>
2022-01-11 00:20:51 +0300

    добавлена генерация айтемов по одному в том же сообщении

--- main.py
+++ main.py
@@ -21,14 +21,42 @@ def send_welcome(message):
 
 
 @bot.message_handler(content_types=['text'])
-def send_comand(message):
-    if(message.text == 'Рандом'):
+def send_command(message):
+    if message.text == 'Рандом':
         # после 303 идут лесные и ивентовые айтемы
         item_num = randint(0, 303)
         while api.items[item_num].startswith("Рецепт"):
             item_num += 1
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        keyboard.add(telebot.types.InlineKeyboardButton(text="Добавить предмет",
+                                                        callback_data='add_item_1'))
         bot.send_message(
-            message.chat.id, api.heroes[randint(0, len(api.heroes) - 1)] + " через " + api.items[item_num])
+            message.chat.id, api.heroes[randint(0, len(api.heroes) - 1)] + " через\n1 " + api.items[item_num],
+            reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data.startswith("add_item"):
+        count = int(call.data[-1])
+        if count < 6:
+            keyboard = telebot.types.InlineKeyboardMarkup()
+            keyboard.add(telebot.types.InlineKeyboardButton(text="Добавить предмет",
+                                                            callback_data='add_item_' + str(count + 1)))
+            item_num = randint(0, 303)
+            while api.items[item_num].startswith("Рецепт"):
+                item_num += 1
+            bot.edit_message_text(chat_id=call.message.chat.id,
+                                  message_id=call.message.id,
+                                  text=call.message.text + "\n" + str(count + 1) + " " + api.items[item_num],
+                                  reply_markup=keyboard)
+        if count == 5:
+            item_num = randint(0, 303)
+            while api.items[item_num].startswith("Рецепт"):
+                item_num += 1
+            bot.edit_message_text(chat_id=call.message.chat.id,
+                                  message_id=call.message.id,
+                                  text=call.message.text + "\n" + str(count + 1) + " " + api.items[item_num])
 
 
 bot.polling(non_stop=True)
